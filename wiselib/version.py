31a2a4b7a03346abc0e7c89bc78849fdd129ab85
Luca Rebuffi <luca.rebuffi@elettra.eu>
2016-09-28 17:48:38 +0200

    General bug fixing (hew and roughness) and layouting

--- wiselib/version.py
+++ wiselib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.2'
 version = '1.0.2'
-full_version = '1.0.2.dev0+4b8ab31'
-git_revision = '4b8ab31950c266cca1f0f9fcf8fed9747aed9218'
+full_version = '1.0.2.dev0+2a16a25'
+git_revision = '2a16a251a6f18b037cbb4963ab8f6ef13a94638d'
 release = False
 
 if not release:
