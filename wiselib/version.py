3bf1b7df769d9aa9de12f46689f0dd656081d733
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-03-16 15:41:47 +0100

    OASYS 1.0.0

--- wiselib/version.py
+++ wiselib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.3'
 version = '1.0.3'
-full_version = '1.0.3.dev0+a55f542'
-git_revision = 'a55f542b3d0b911806b8112f54172f8d08d5829b'
+full_version = '1.0.3.dev0+52637c2'
+git_revision = '52637c232da315ebf300b82872f907490f5f2afe'
 release = False
 
 if not release:
