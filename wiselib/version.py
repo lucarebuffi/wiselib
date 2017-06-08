a2008e4d3d37d2af64f90d2bc5583d2a1680d302
Luca Rebuffi <luca.rebuffi@elettra.eu>
2017-06-08 13:26:22 +0200

    Bug fixing and Issues

--- wiselib/version.py
+++ wiselib/version.py
@@ -1,9 +1,9 @@
 
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
-short_version = '1.0.3'
-version = '1.0.3'
-full_version = '1.0.3.dev0+52637c2'
-git_revision = '52637c232da315ebf300b82872f907490f5f2afe'
+short_version = '1.0.4'
+version = '1.0.4'
+full_version = '1.0.4.dev0+c3272d7'
+git_revision = 'c3272d77ceba224a602285287e12165bddd14724'
 release = False
 
 if not release:
