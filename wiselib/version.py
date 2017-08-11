65c4724ea74e7e6d3d7acff899cbaa7aac82d267
luca.rebuffi@elettra.eu <Bacilotto_00>
2017-08-10 17:31:39 -0700

    Blocking installation of Release add-ons into Beta

--- wiselib/version.py
+++ wiselib/version.py
@@ -2,8 +2,8 @@
 # THIS FILE IS GENERATED FROM OASYS SETUP.PY
 short_version = '1.0.5'
 version = '1.0.5'
-full_version = '1.0.5.dev0+a2008e4'
-git_revision = 'a2008e4d3d37d2af64f90d2bc5583d2a1680d302'
+full_version = '1.0.5.dev0+12539c1'
+git_revision = '12539c13b6219af24eef9e2c4ad8ca8ead21b261'
 release = False
 
 if not release:
